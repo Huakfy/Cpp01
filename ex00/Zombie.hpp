bf7f53508798f14d54c814571435676cdadbddaa
Marc <marc@DESKTOP-G59U5LS>
2023-05-30 11:30:13 +0200

    init

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 10:59:59 by mjourno           #+#    #+#             */
+/*   Updated: 2023/05/30 11:12:06 by mjourno          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	_name;
+	public:
+		void announce( void );
+};
+
+#endif
