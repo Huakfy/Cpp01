882c2977cf0a8e01d0f7732748fbd80200a5e52e
Marc <Nope>
2023-05-31 20:54:00 +0200

    fin_ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:15:40 by mjourno           #+#    #+#             */
-/*   Updated: 2023/05/31 18:37:09 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/05/31 20:53:15 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,53 @@
 #include <string>
 #include <cstring>
 
-int	main(int ac, char **av) {
-	if (ac != 4)
+std::string	sed(std::string content, std::string s1, std::string s2) {
+	std::string	result = "";
+	for (long unsigned int i = 0; i < content.length(); i++)
 	{
+		if (content[i] == s1[0])
+		{
+			long unsigned int	j = 0;
+			while (content[i + j] == s1[j])
+				j++;
+			if (j == (s1.length()))
+			{
+				result += s2;
+				i += j - 1;
+				continue;
+			}
+		}
+		result += content[i];
+	}
+	return result;
+}
+
+const char	*outputName(char *input) {
+	std::string	outputName = input;
+	outputName += ".replace";
+	const char* str = outputName.c_str();
+	return str;
+}
+
+int	main(int ac, char **av) {
+	if (ac != 4) {
 		std::cout << "Need 3 parameters: filename, s1 and s2" << std::endl;
 		return 1;
 	}
-
-
 	std::ifstream	inputStream (av[1]);
-
-	if (inputStream.is_open())
-	{
-		std::string	outputName = av[1];
-		outputName += ".replace";
-		const char* str = outputName.c_str();
-		std::ofstream	outputStream (str);
-
-		std::string	s1 = av[2];
-		std::string	s2 = av[3];
-
-		std::string	content;
+	if (inputStream.is_open()) {
+		std::ofstream	outputStream (outputName(av[1]));
+		std::string		s1 = av[2];
+		std::string		s2 = av[3];
+		std::string		content;
 		while (inputStream)
 		{
-			std::string	result = "";
 			if (!std::getline(inputStream, content))
 				break;
-			for (long unsigned int i = 0; i < content.length(); i++)
-			{
-				if (content[i] == s1[0])
-				{
-					long unsigned int	j = 0;
-					while (content[i + j] == s1[j])
-						j++;
-					if (j == (s1.length() - 1))
-					{
-						result += s2;
-						i += j;
-						continue;
-					}
-				}
-				result += content[i];
-			}
-			outputStream << result << std::endl;
+			outputStream << sed(content, s1, s2) << std::endl;
 		}
 	}
-	else {
+	else
 		std::cout << "Couldn't open file\n";
-	}
-
 	return 0;
 }
