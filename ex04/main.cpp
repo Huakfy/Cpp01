8d5701c32dca5a1d62fe1e3b2141824468960e2e
Marc <Nope>
2023-05-31 18:39:38 +0200

    working

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,14 +6,13 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:15:40 by mjourno           #+#    #+#             */
-/*   Updated: 2023/05/31 14:33:57 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/05/31 18:37:09 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-
 #include <cstring>
 
 int	main(int ac, char **av) {
@@ -31,15 +30,34 @@ int	main(int ac, char **av) {
 		std::string	outputName = av[1];
 		outputName += ".replace";
 		const char* str = outputName.c_str();
-
 		std::ofstream	outputStream (str);
+
+		std::string	s1 = av[2];
+		std::string	s2 = av[3];
 
 		std::string	content;
 		while (inputStream)
 		{
+			std::string	result = "";
 			if (!std::getline(inputStream, content))
 				break;
-			outputStream << content << std::endl;
+			for (long unsigned int i = 0; i < content.length(); i++)
+			{
+				if (content[i] == s1[0])
+				{
+					long unsigned int	j = 0;
+					while (content[i + j] == s1[j])
+						j++;
+					if (j == (s1.length() - 1))
+					{
+						result += s2;
+						i += j;
+						continue;
+					}
+				}
+				result += content[i];
+			}
+			outputStream << result << std::endl;
 		}
 	}
 	else {
